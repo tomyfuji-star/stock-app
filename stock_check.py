b05be5ff980b4ecb5b88edab1ffb595c9edc177a
fuji <ken@fuji-3.local>
2025-12-28 09:28:30 +0900

    show stock list

--- stock_check.py
+++ stock_check.py
@@ -3,15 +3,34 @@ import os
 
 app = Flask(__name__)
 
+stocks = [
+    {"code": "7203.T", "name": "トヨタ", "buy_price": 2500, "shares": 100},
+    {"code": "6758.T", "name": "ソニー", "buy_price": 12000, "shares": 50},
+]
+
 @app.route("/")
 def index():
-    return """
-    <h1>株価アプリ 起動成功</h1>
-    <p>Renderで正常に動いています</p>
+    html = """
+    <h1>保有株一覧</h1>
+    <table border="1" cellpadding="5">
+      <tr>
+        <th>銘柄</th>
+        <th>コード</th>
+        <th>取得単価</th>
+        <th>株数</th>
+      </tr>
     """
+    for s in stocks:
+        html += f"""
+        <tr>
+          <td>{s['name']}</td>
+          <td>{s['code']}</td>
+          <td>{s['buy_price']}</td>
+          <td>{s['shares']}</td>
+        </tr>
+        """
+    html += "</table>"
+    return html
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
-    )
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
